e5984946bd84a8256ebe0297bac81b9255243b97
paramdoit1 <paramdoit1@gmail.com>
2024-08-05 08:36:12 -0700

    added core python

---
+++ Tutorials/python/core/1_comments.py
@@ -0,0 +1,15 @@
+#Single line comments
+
+# This is single line comment
+
+'''
+This is multi line 
+comments in python
+'''
+
+"""
+This is multi line 
+comments in python
+"""
+
+print('comments in python')
